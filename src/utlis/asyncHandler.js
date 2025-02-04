1a6c9e3c8df06c8d08d327c65436a86adaa3f2fd
Ankur Kshitij Sahai <ankurkshitijsahai@gmail.com>
2025-02-05 01:57:56 +0530

    all setup configured

--- src/utlis/asyncHandler.js
+++ src/utlis/asyncHandler.js
@@ -1,21 +1,23 @@
-const asyncHandler= (func)=> async (req, res, next)=>{
-    try {
-        await func(req, res, next);
-    } catch (error) {
-        res.status(err.code || 500).json({
-            success: false,
-            message: err.message
-        })
-    }
-}
+// const asyncHandler= (func)=> async (req, res, next)=>{
+//     try {
+//         await func(req, res, next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
 
-export {asyncHandler}
+// export {asyncHandler}
 
 // above code or the below code for handeling the async handler
 
 
-// const asyncHandler = (requestHandler) => {
-//     return (req, res, next) => {
-//         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-//     }
-// }
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+export {asyncHandler}
+
