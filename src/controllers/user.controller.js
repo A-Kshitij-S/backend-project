b8c7d37fa54fc8972fbb76f537c3c2ee24ed2618
A-Kshitij-S <ankurkshitijsahai22@gmail.com>
2025-03-16 09:33:06 +0530

    fixed register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,24 +5,30 @@ import {uploadOnCloudinary} from "../utlis/cloudinary.js"
 import {ApiResponse} from "../utlis/apiResponse.js"
 
 const registerUser= asyncHandler( async (req, res)=>{
-    const {fullname, email, username, password}=req.body
+    const {fullName, email, username, password}=req.body
 
     if(
-        [fullname, email, username, password].some((e)=>
-        e.trim()==="")
+        [fullName, email, username, password].some((e)=>
+        e?.trim()==="")
     ){
         throw new ApiError(400, "All fields are required")
     }
 
-    const checkUser= User.findOne({
+    const checkUser= await User.findOne({
         $or: [{username}, {email}]
     })
     if(checkUser){
         throw new ApiError(409, "username or email already exist")
     }
  
-    const avatarLocalPath= req.files?.avatar[0]?.path
-    const coverImageLocalPath= req.files?.coverImage[0]?.path
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // const coverImageLocalPath= req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && 
+    req.files.coverImage.length()>0){
+        coverImageLocalPath= req.files.coverImage[0].path;  
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
@@ -36,7 +42,7 @@ const registerUser= asyncHandler( async (req, res)=>{
     }
 
     const user= await User.create({
-        fullname,
+        fullName,
         avatar:avatar.url,
         coverImage: coverImage?.url || "",
         email,
@@ -44,7 +50,7 @@ const registerUser= asyncHandler( async (req, res)=>{
         username: username.toLowerCase()
     })
 
-    const createdUser= await user.findById(user._id).select(
+    const createdUser= await User.findById(user._id).select(
         "-password -refreshToken"
     )
     
@@ -52,8 +58,8 @@ const registerUser= asyncHandler( async (req, res)=>{
         throw new ApiError(500, "something went wrong in servers while creating user")
     }
     
-    return req.status(200).json(
-        new ApiResponse(200, "user created successfully")
+    return res.status(200).json(
+        new ApiResponse(200,createdUser, "user created successfully")
     )
 })
 
